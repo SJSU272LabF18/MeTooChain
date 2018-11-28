62496b341d4910cb1948d98b8533d00f48a58e1e
kunal pavashiya <kunalp726@gmail.com>
2018-11-28 03:34:51 -0700

    message

--- backend/controllers/requestconsent.js
+++ backend/controllers/requestconsent.js
@@ -1,51 +1,80 @@
 var User = require("../models/user");
 exports.requestconsent = function(req, res) {
-  var receivedobj = {
-    sendername: "Stacy", //req.body.sendername
-    preference: "fling", //req.body.preference
-    level: 2, //req.body.level
-    status: 1 //1. req.body.status
+  var sentObject  = {
+    sendername: req.body.sendername,
+    preference: req.body.preference,
+    level: req.body.level,
+    status: "Pending"
   };
-  var sentObject = {
-    receivername: "sojan", //req.body.receivername
-    preference: "fling", //req.body.preference
-    level: 2, //req.body.level
-    status: 1 //req.body.status
+  var receivedobj = {
+    receivername: req.body.receivername,
+    preference: req.body.preference,
+    level: req.body.level,
+    status:"Pending"
   };
 
-  User.findOneAndUpdate(
-    { "user.username": "sojan" },
-    { $push: { requests: receivedobj } },
-    function(err, doc) {
-      if (err) {
-        res.code = "400";
-        res.value = "Cannot request consent at moment";
-        console.log(res.value);
-        res.status(400).send({ message: "Cannot request consent at moment" });
-      } else {
-        User.findOneAndUpdate(
-          { "user.username": "Stacy" },
-          { $push: { sentrequests: sentObject } },
-          function(err, doc) {
-            if (err) {
-              res.code = "400";
-              res.value = "Could not give consent";
-              console.log(res.value);
-              res.status(400).send({ message: "could not give consent" });
-            } else {
-              res.code = "200";
-              res.value = doc;
-              console.log(doc);
-              res.status(200).send({ message: "successfully given consent" });
-            }
-          }
-        );
+  
+console.log(JSON.stringify(req.body));
+
+
+var query=User.findOneAndUpdate(
+    { "user.username": req.body.receivername },
+    { $push: { requests: sentObject } });
+    query.then(User.findOneAndUpdate(
+      { "user.username": req.body.sendername },
+      { $push: { sentrequests: receivedobj } },
+      function(err, doc) {
+        if (err) {
+          res.code = "400";
+          res.value = "Could not give consent";
+          console.log(res.value);
+          res.status(400).send({ message: "could not give consent" });
+        } else {
+          res.code = "200";
+          res.value = doc;
+          console.log(doc);
+          res.status(200).send({ message: "successfully given consent" });
+        }
       }
-    }
-  ).catch(function(err) {
-    // just need one of these
-    console.log("error:", err.message);
-    res.code = "400";
-    callback(err, res);
-  });
+    )).catch(function(err) {
+        console.log("error:", err.message);
+        res.code = "400";
+      });
+
+    
+  // User.findOneAndUpdate(
+  //   { "user.username": req.body.receivername },
+  //   { $push: { requests: sentObject } },
+  //   function(err, doc) {
+  //     if (err) {
+  //       res.code = "400";
+  //       res.value = "Cannot request consent at moment";
+  //       console.log(res.value);
+  //       res.status(400).send({ message: "Cannot request consent at moment" });
+  //     } else {
+  //       User.findOneAndUpdate(
+  //         { "user.username": req.body.sendername },
+  //         { $push: { sentrequests: receivedobj } },
+  //         function(err, doc) {
+  //           if (err) {
+  //             res.code = "400";
+  //             res.value = "Could not give consent";
+  //             console.log(res.value);
+  //             res.status(400).send({ message: "could not give consent" });
+  //           } else {
+  //             res.code = "200";
+  //             res.value = doc;
+  //             console.log(doc);
+  //             res.status(200).send({ message: "successfully given consent" });
+  //           }
+  //         }
+  //       );
+  //     }
+  //   }
+  // ).catch(function(err) {
+  //   // just need one of these
+  //   console.log("error:", err.message);
+  //   res.code = "400";
+  //   callback(err, res);
+  // });
 };
